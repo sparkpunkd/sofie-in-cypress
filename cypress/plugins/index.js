9f5728b3d581a1fcc1b1ec7f91f2621e554a52c8
Richard Cartwright <spark@streampunk.media>
2020-08-24 18:45:50 +0100

    feature: add user log take execution time and don't leak events

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,7 @@
 const dgram = require('dgram')
+const got = require('got')
 
-const casparSocket = dgram.createSocket({ type: 'udp4', reuseAddr: true }).bind(52500)
+const casparSocket = dgram.createSocket({ type: 'udp4', reuseAddr: true }).bind(52500);
 const atemSocket = dgram.createSocket({ type: 'udp4', reuseAddr: true }).bind(52502);
 
 module.exports = (on, config) => {
@@ -11,13 +12,25 @@ module.exports = (on, config) => {
       console.log('Waiting for caspar to', lookFor)
       let start = process.hrtime()
 
-      return new Promise((resolve) => {
+      return new Promise((resolve, reject) => {
+        let eventTimer = setTimeout(() => {
+          casparSocket.removeAllListeners();
+          reject(new Error('Failed to receive looked for message from Caspar in time'));
+        }, config.taskTimeout);
         casparSocket.on('message', (m) => {
           let ms = m.toString();
-          console.log('Received from Caspar:', ms);
+          console.log('Received from caspar:', ms);
           if (ms.indexOf(lookFor) >= 0) {
+            let endTime = process.hrtime(start);
+            clearTimeout(eventTimer);
             casparSocket.removeAllListeners();
-            resolve({ matched: ms, timing: process.hrtime(start) });
+            got(`${config.env['host']}/api/0/publication/userActionsLog/%7B%7D/undefined`)
+              .json()
+              .then(actions => {
+                let lastAct = actions.reverse().find(x => x.method === 'userAction.take');
+                resolve({ lookFor, matched: ms, timing: endTime, takeExecutionTime: lastAct.executionTime });
+              },
+              reject);
           }
         })
       })
@@ -26,14 +39,25 @@ module.exports = (on, config) => {
       console.log('Waiting for atem to', lookFor)
       let start = process.hrtime()
 
-      return new Promise((resolve) => {
-        console.log('Wakey wakey');
+      return new Promise((resolve, reject) => {
+        let eventTimer = setTimeout(() => {
+          casparSocket.removeAllListeners();
+          reject(new Error('Failed to receive looked for command sent to Atem in time'));
+        }, config.taskTimeout);
         atemSocket.on('message', (m) => {
           let ms = m.toString();
           console.log('Received from Atem:', ms);
           if (ms.indexOf(lookFor) >= 0) {
+            let endTime = process.hrtime(start);
+            clearTimeout(eventTimer);
             atemSocket.removeAllListeners();
-            resolve({ matched: ms, timing: process.hrtime(start) });
+            got(`${config.env['host']}/api/0/publication/userActionsLog/%7B%7D/undefined`)
+            .json()
+            .then(actions => {
+              let lastAct = actions.reverse().find(x => x.method === 'userAction.take');
+              resolve({ lookFor, matched: ms, timing: endTime, lastTake: lastAct });
+            },
+            reject);
           }
         })
       })
